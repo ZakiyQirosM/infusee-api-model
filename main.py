f1e0348dd00a2aa448a2d4786b49503c80d11a7e
ZakiyQirosM <d0r0i0ver11@gmail.com>
2025-05-21 14:46:31 +0700

    pebaikan pengambilan data prediksi dan penambahan range data yang bisa dipredik

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import mysql.connector
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
-import numpy as np
 import pandas as pd
 
 app = FastAPI()
@@ -10,6 +9,9 @@ app = FastAPI()
 # Load model
 model_volume = joblib.load("model_volume.pkl")
 model_tpm = joblib.load("model_tpm.pkl")
+
+min_berat_total = 100
+max_berat_total = 2000
 
 # Koneksi ke MySQL (XAMPP)
 def get_db_connection():
@@ -26,61 +28,57 @@ class IDInput(BaseModel):
 @app.post("/prediksi-dari-db")
 def prediksi_tpm_dari_db(data: IDInput):
     print("DEBUG: Request diterima dengan data:", data.dict())
-    # Fungsi untuk prediksi TPM
-    def prediksi_tpm(volume, durasidb, umur):
-        input_data = pd.DataFrame([{
-            'Volume Infus (ml)': volume,    
-            'Durasi': durasidb,
-            'Umur': umur
-        }])
-        tpm = model_tpm.predict(input_data)[0]
-        return float(tpm)
-    
-    conn = get_db_connection()
-    cursor = conn.cursor()
 
-    query = """
-        SELECT 
-            m.berat_total, 
-            s.durasi_infus_jam, 
-            u.umur
-        FROM table_monitoring_infus m
-        JOIN infusion_sessions s ON m.id_session = s.id_session
-        JOIN table_pasien u ON s.no_reg_pasien = u.no_reg_pasien
-        WHERE m.id_session = %s
-        ORDER BY m.waktu DESC
-        LIMIT 1;
-    """
-    cursor.execute(query, (data.id_session,))
-    result = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
+    # Ambil data dari database
+    with get_db_connection() as conn:
+        with conn.cursor() as cursor:
+            query = """
+                SELECT 
+                    m.berat_total, 
+                    s.durasi_infus_jam, 
+                    u.umur
+                FROM table_monitoring_infus m
+                JOIN infusion_sessions s ON m.id_session = s.id_session
+                JOIN table_pasien u ON s.no_reg_pasien = u.no_reg_pasien
+                WHERE m.id_session = %s
+                ORDER BY m.waktu DESC
+                LIMIT 1;
+            """
+            cursor.execute(query, (data.id_session,))
+            result = cursor.fetchone()
 
     if not result:
         return {"error": "Data tidak ditemukan untuk id_session tersebut"}
 
     berat_total, durasidb, umur = result
 
+    if berat_total < min_berat_total or berat_total > max_berat_total:
+        return {"error": f"Berat total {berat_total} di luar jangkauan training data"}
+    
     # Prediksi volume dari berat_total
-    volume = model_volume.predict(np.array([[berat_total]]))[0]
+    input_volume_df = pd.DataFrame([{
+    "Berat Total (g)": berat_total
+    }])
+    volume = model_volume.predict(input_volume_df)[0]
 
-    # Prediksi TPM dari volume, durasidb, umur
-    tpm = int(round(prediksi_tpm(volume, durasidb, umur)))
+    # Prediksi TPM
+    input_tpm_df = pd.DataFrame([{
+        'Volume Infus (ml)': volume,
+        'Durasi': durasidb,
+        'Umur': umur
+    }])
+    tpm = int(round(model_tpm.predict(input_tpm_df)[0]))
 
-    # Simpan ke DB
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    update_query = """
-        UPDATE table_monitoring_infus
-        SET tpm_prediksi = %s
-        WHERE id_session = %s;
-    """
-    cursor.execute(update_query, (tpm, data.id_session))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    # Update TPM ke database
+    with get_db_connection() as conn:
+        with conn.cursor() as cursor:
+            update_query = """
+                UPDATE table_monitoring_infus
+                SET tpm_prediksi = %s
+                WHERE id_session = %s;
+            """
+            cursor.execute(update_query, (tpm, data.id_session))
+            conn.commit()
 
     return {
         "berat_total": berat_total,
